ed9e16519c3b2aa17c58d590c17d0c48dc777044
Shiangan <avarubby85110@gmail.com>
2026-02-19 11:59:58 +0800

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<header class="main-header" id="navbar">
+        <div class="container nav-wrapper">
+            <div class="logo">
+                <a href="index.html"><img src="images/logo.png" alt="祥安生命"></a>
+            </div>
+            
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="index.php">首頁</a></li>
+                    <li><a href="about.php">關於祥安</a></li>
+                    
+                    <li>
+                        <a href="#">專業生命禮儀 <i class="fas fa-chevron-down" style="font-size: 0.7rem;"></i></a>
+                        <ul class="submenu">
+                            <li class="submenu-title">依信仰分類</li>
+                            <li><a href="services.php#buddhist-taoist">中式禮儀 (一般習俗/佛教/道教)</a></li>
+                            <li><a href="services.php#western-etiquette">西式禮儀 (基督教/天主教)</a></li>
+                            <li><a href="services.php#japanese-etiquette">日式禮儀 (創價學會/日蓮正宗)</a></li>
+                            <li class="submenu-title">現代環保</li>
+                            <li><a href="services.php#eco-burial">環保葬 (樹/花/海葬)</a></li>
+                            <li><a href="services.php#customized-etiquette">客製化禮儀服務</a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">喪葬費用方案 <i class="fas fa-chevron-down" style="font-size: 0.7rem;"></i></a>
+                        <ul class="submenu">
+                            <li class="submenu-title">公開透明契約</li>
+                            <li><a href="plans.html#basic">國民契約 6.8萬</a></li>
+                            <li><a href="plans.html#eighty-eight">中式契約 8.8萬起</a></li>
+                            <li><a href="plans.html#seventeen-eight">尊榮契約 17.8萬</a></li>
+                            <li class="submenu-title">其它類別</li>
+                            <li><a href="plans.html#west">西式契約方案</a></li>
+                            <li><a href="plans.html#japen">日式契約方案</a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">殯葬百科 <i class="fas fa-chevron-down" style="font-size: 0.7rem;"></i></a>
+                        <ul class="submenu">
+                            <li><a href="blog.php#first-step">臨終/初終處理</a></li>
+                            <li><a href="blog.php#law-guide">法規與補助指南</a></li>
+                            <li><a href="ta-wei-he-fa-xing-query-guide.php">塔位合法性查詢</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="order.php">花禮訂購</a></li>
+                </ul>
+            </nav>
+            <div class="menu-toggle"><i class="fas fa-bars"></i></div>
+        </div>
+    </header>
